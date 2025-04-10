2e2d72e19b30ad7eb36c7244c7e7363641f0fd4c
manish <manish.prajapat94@gmail.com>
2025-04-10 16:47:16 +0530

    add review section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Footer from "./components/footer";
+import ReviewSection from "./components/review_section";
 
 function App() {
   return (
     <>
+      <ReviewSection />
       <Footer />
     </>
   );
