dded14e1463703f41961c20986a0908639d51f52
Manishh94 <manishprajapat94@gmail.com>
2025-04-10 19:07:41 +0530

    implement responsivness on second container image

--- src/components/loginDivComponent.jsx
+++ src/components/loginDivComponent.jsx
@@ -2,16 +2,27 @@ export default function LoginContainer() {
   return (
     <>
       <div className="container-fluid p-5 ">
-        <div className="row login-container-div ps-3">
-          <div className="col-6 d-flex justify-content-end">
-            <div className="login-text-div mt-4 ">
-              <h3 className="fw-bolder me-4">Search Properti,</h3>
-              <h3 className="fw-bolder">List Your Property</h3>
-              <p>Get Started Now</p>
-            </div>
+        <div className="row login-container-row">
+          
+          <div className="col-lg-6 col-md-6 col-sm-12 order-md-2">
+            <img
+              className="logincontainer-image img-fluid"
+              src="./logincontainer-image.jpg"
+              alt=""
+            />
           </div>
-          <div className="col-6 ">
-            <img className="logincontainer-imag" src="/logincontainer-imag.jpg" alt="" />
+          <div className="col-lg-6 col-md-6 col-sm-12 d-flex order-md-1 justify-content-md-end">
+            <div className="login-text-div mt-4 pt-5">
+              <h4 className="fw-bolder me-4">
+                Search Properti,
+                <br />
+                List Your Property
+              </h4>
+              <p>Get Started Now</p>
+              <button type="button" class="btn btn-outline-primary w-50">
+                Login
+              </button>
+            </div>
           </div>
         </div>
       </div>
